8151027465758ac3a9239b327465faf954aff626
Akinyi-54 <gracefiona236@gmail.com>
2025-02-20 02:00:05 +0300

    First Commit on ChatBot-App

---
+++ src/main/java/com/springboot/chatbot/Chatbot_App/ChatbotAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.chatbot.Chatbot_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbotAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbotAppApplication.class, args);
+	}
+
+}
